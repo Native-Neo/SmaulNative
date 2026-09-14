6f00c8943384f724baa438a4098e72479fafb016
NativeMc <yashyt6246@gmail.com>
2026-09-14 21:53:34 +0530

    Version semantic checkpoint format

--- src/checkpoint_version.rs
+++ src/checkpoint_version.rs
@@ -1,6 +1,6 @@
-pub const CHECKPOINT_FORMAT_VERSION:u32=1;
+pub const CHECKPOINT_FORMAT_VERSION:u32=2;
 #[derive(Clone,Copy,Debug,PartialEq,Eq)]
 pub struct CheckpointVersion(pub u32);
 impl CheckpointVersion{pub fn current()->Self{Self(CHECKPOINT_FORMAT_VERSION)}pub fn is_supported(self)->bool{self.0==CHECKPOINT_FORMAT_VERSION}}
 #[cfg(test)]
-mod tests{use super::*;#[test]fn current_is_supported(){assert!(CheckpointVersion::current().is_supported());}}
+mod tests{use super::*;#[test]fn current_is_supported(){assert!(CheckpointVersion::current().is_supported());assert_eq!(CheckpointVersion::current().0,2);}}
