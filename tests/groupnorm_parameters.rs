860a12426307f5ee5f48e8c481b477feaa8ef272
Yash <yashyt6246@gmail.com>
2026-09-16 18:37:43 +0530

    Delete tests/groupnorm_parameters.rs

--- tests/groupnorm_parameters.rs
+++
@@ -1,7 +0,0 @@
-use smaul_native::group_norm::GroupNorm;
-
-#[test]
-fn group_norm_parameter_count_matches_affine_vectors() {
-    let norm = GroupNorm::new(12, 3, 1e-5);
-    assert_eq!(norm.parameter_count(), 24);
-}
