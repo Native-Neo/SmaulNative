876f73c1f4e9f3ef3e2d67fc59b46349b6fb572f
Yash <yashyt6246@gmail.com>
2026-09-16 18:37:43 +0530

    Delete tests/embedding_shape.rs

--- tests/embedding_shape.rs
+++
@@ -1,7 +0,0 @@
-use smaul_native::embedding::Embedding;
-
-#[test]
-fn embedding_preserves_token_sequence_shape() {
-    let embedding = Embedding::new(16, 8, 7);
-    assert_eq!(embedding.forward(&[0, 3, 15]).dim(), (3, 8));
-}
