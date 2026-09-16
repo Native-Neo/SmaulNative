d16778fdde874c48d0b5d3c731e980bd6d0c3d79
Yash <yashyt6246@gmail.com>
2026-09-16 18:37:43 +0530

    Delete tests/linear_layout.rs

--- tests/linear_layout.rs
+++
@@ -1,8 +0,0 @@
-use ndarray::array;
-use smaul_native::linear::Linear;
-
-#[test]
-fn linear_matrix_layout_is_stable() {
-    let layer = Linear::from_weight(array![[1.0, 2.0], [3.0, 4.0]]);
-    assert_eq!(layer.forward(&array![[2.0, 3.0]]), array![[8.0, 18.0]]);
-}
