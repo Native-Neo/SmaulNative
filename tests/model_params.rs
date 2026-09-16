3870feb05230037fbdab0b109bfde80946a1ad5e
Yash <yashyt6246@gmail.com>
2026-09-16 18:37:43 +0530

    Delete tests/model_params.rs

--- tests/model_params.rs
+++
@@ -1,7 +0,0 @@
-use smaul_native::rwkv_model::{RwkvModel, RwkvModelConfig};
-
-#[test]
-fn model_has_parameters() {
-    let model = RwkvModel::new(RwkvModelConfig::new(32, 16, 2, 4), 123);
-    assert!(model.parameter_count() > 0);
-}
