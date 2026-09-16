6e622a74dda1307a933119a4b8ca4bd73353158f
Yash <yashyt6246@gmail.com>
2026-09-16 18:37:43 +0530

    Delete tests/model_moba_config.rs

--- tests/model_moba_config.rs
+++
@@ -1,9 +0,0 @@
-use smaul_native::rwkv_model::RwkvModelConfig;
-
-#[test]
-fn model_moba_configuration_is_recorded() {
-    let config = RwkvModelConfig::new(32, 16, 5, 4).with_moba(2, 8, 1);
-    assert_eq!(config.n_moba_layer, 2);
-    assert_eq!(config.moba_chunk_size, 8);
-    assert_eq!(config.moba_topk, 1);
-}
