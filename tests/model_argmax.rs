d9f2483dcbb3762ad154a348a629eca6d5737d01
Yash <yashyt6246@gmail.com>
2026-09-16 18:37:43 +0530

    Delete tests/model_argmax.rs

--- tests/model_argmax.rs
+++
@@ -1,8 +0,0 @@
-use ndarray::array;
-use smaul_native::rwkv_model::RwkvModel;
-
-#[test]
-fn model_argmax_returns_vocab_indices() {
-    let logits = array![[0.0, 4.0, 2.0], [7.0, 1.0, 6.0]];
-    assert_eq!(RwkvModel::argmax(&logits).to_vec(), vec![1, 0]);
-}
