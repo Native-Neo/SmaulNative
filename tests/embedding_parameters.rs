adfc8e3c5a9582e783d076f876c868b5e1562a97
Yash <yashyt6246@gmail.com>
2026-09-16 18:37:43 +0530

    Delete tests/embedding_parameters.rs

--- tests/embedding_parameters.rs
+++
@@ -1,8 +0,0 @@
-use smaul_native::embedding::Embedding;
-
-#[test]
-fn embedding_parameter_count_matches_storage() {
-    let embedding = Embedding::new(10, 6, 9);
-    assert_eq!(embedding.parameter_count(), 60);
-    assert_eq!(embedding.flatten_weight().len(), 60);
-}
