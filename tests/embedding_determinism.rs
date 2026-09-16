0b97182775313101f7b09f5af021be8df2eb01f9
Yash <yashyt6246@gmail.com>
2026-09-16 18:37:43 +0530

    Delete tests/embedding_determinism.rs

--- tests/embedding_determinism.rs
+++
@@ -1,8 +0,0 @@
-use smaul_native::embedding::Embedding;
-
-#[test]
-fn embedding_initialization_is_seeded() {
-    let a = Embedding::new(8, 4, 123);
-    let b = Embedding::new(8, 4, 123);
-    assert_eq!(a.weight, b.weight);
-}
