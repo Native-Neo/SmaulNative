1bf7af67bcef35d711d9de6e6bddb8a4906b7435
Yash <yashyt6246@gmail.com>
2026-09-16 18:37:43 +0530

    Delete tests/init_range.rs

--- tests/init_range.rs
+++
@@ -1,7 +0,0 @@
-use smaul_native::init::uniform;
-
-#[test]
-fn uniform_initialization_respects_bounds() {
-    let x = uniform(16, 8, -0.25, 0.5, 31);
-    assert!(x.iter().all(|v| *v >= -0.25 && *v <= 0.5));
-}
