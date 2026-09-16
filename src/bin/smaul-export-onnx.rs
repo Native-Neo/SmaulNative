3d9295aae638369bd1747bfb1fed6d754744010f
Yash <yashyt6246@gmail.com>
2026-09-16 18:37:42 +0530

    Delete src/bin/smaul-export-onnx.rs

--- src/bin/smaul-export-onnx.rs
+++
@@ -1,9 +0,0 @@
-use smaul_native::onnx_export::export_cli;
-
-fn main() {
-    let args: Vec<String> = std::env::args().collect();
-    if let Err(error) = export_cli(&args) {
-        eprintln!("error: {error}");
-        std::process::exit(1);
-    }
-}
