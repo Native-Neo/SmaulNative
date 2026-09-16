8e15c43ac9c719b6e328e58c04ff47d1cd9054c9
Yash <yashyt6246@gmail.com>
2026-09-16 18:37:43 +0530

    Delete tests/linear_parameter_count.rs

--- tests/linear_parameter_count.rs
+++
@@ -1,7 +0,0 @@
-use smaul_native::linear::Linear;
-
-#[test]
-fn linear_parameter_count_tracks_bias() {
-    assert_eq!(Linear::new(5, 7).parameter_count(), 35 + 7);
-    assert_eq!(Linear::new_no_bias(5, 7).parameter_count(), 35);
-}
